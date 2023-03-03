dc5bf0110e23a8c7fe3477f3ff5f063606678183
Davi Holanda <daviht7@gmail.com>
2023-03-03 16:50:57 -0300

    estrutura da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,38 @@
 import { styled } from "@/styles";
+import Image from "next/image";
+import { HomeContainer, Product } from '../styles/pages/home';
 
-const Button = styled('button', {
-
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-
-})
+import camiseta1 from '../assets/camisas/1.png'
+import camiseta2 from '../assets/camisas/2.png'
+import camiseta3 from '../assets/camisas/3.png'
 
 export default function Home() {
   return (
-    <>
-      <Button>
-        <span>Teste</span>
-        Enviar
-      </Button>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="camiseta 1" />
+
+        <footer>
+          <strong>camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="camiseta 2" />
+
+        <footer>
+          <strong>camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta3} width={520} height={480} alt="camiseta 3" />
+
+        <footer>
+          <strong>camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
