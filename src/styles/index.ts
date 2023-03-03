c0dba06c27595b537992b02bea9a166e91f85082
Davi Holanda <daviht7@gmail.com>
2023-03-03 15:38:53 -0300

    estilos globais

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,9 +4,17 @@ export const { config, styled, globalCss, keyframes, getCssText, theme, createTh
 
   theme: {
     colors: {
-      rocketseat: '#8257e6'
+      white: '#FFF',
+
+      gray900: '#121212',
+      gray800: '#202424',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e'
+
     }
   }
 
 })
-
