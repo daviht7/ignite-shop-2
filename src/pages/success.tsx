ebbba429ede283d2dfb76da394986494237cc94f
Davi Holanda <daviht7@gmail.com>
2023-03-07 19:24:38 -0300

    estrutura de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,24 @@
+
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <>
-      <h1>Success</h1>
-    </>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        uhull <strong>Davi Holanda</strong>, sua camiseta <strong>Beyond</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+
+    </SuccessContainer>
   )
 }
